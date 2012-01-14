21587fad79d4c43648a355190b6b175faa321967
Michael L Perry <mperry@adventuresinsoftware.com>
2012-01-14 11:22:46 -0600

    Added gift card animation.

---
+++ HistoricalModelingPresentation/Assets/CashRegister.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace HistoricalModelingPresentation
+{
+	public partial class CashRegister : UserControl
+	{
+		public CashRegister()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
