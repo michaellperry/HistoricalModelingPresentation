2c272ae61007bf409e91ca78a1dc52b0ad914dca
Michael L Perry <mperry@adventuresinsoftware.com>
2012-01-14 22:39:49 -0600

    Completed fulfillment example animation.

--- HistoricalModelingPresentation/ViewModels/FulfillmentViewModel.cs
+++ HistoricalModelingPresentation/ViewModels/FulfillmentViewModel.cs
@@ -1,21 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using ThoughtCloud_Presentation.ViewModels;
+﻿using ThoughtCloud_Presentation.ViewModels;
+using UpdateControls.Fields;
 
 namespace HistoricalModelingPresentation.ViewModels
 {
     public class FulfillmentViewModel : IPresentationViewModel
     {
-        public bool Backward()
+        public enum StateId
         {
-            return false;
+            Start,
+            PlaceOrder,
+            PlaceCustomer,
+            PlaceCompany,
+            PlaceOrderB,
+            PlaceCustomer2,
+            BackToOne,
+            ShowShipment,
+            PlaceShipment,
+            ShowInvoice,
+            PlaceInvoice,
+            ShowDelivery,
+            PlaceDelivery,
+            ShowPayment,
+            PlacePayment,
+            ShowFollowUp,
+            PlaceFollowUp,
+            End
+        }
+
+        private Independent<StateId> _state = new Independent<StateId>();
+
+        public StateId State
+        {
+            get { return _state; }
         }
 
         public bool Forward()
         {
-            return false;
+            if (_state.Value != StateId.End - 1)
+            {
+                _state.Value = _state.Value + 1;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool Backward()
+        {
+            if (_state.Value != StateId.Start)
+            {
+                _state.Value = _state.Value - 1;
+                return true;
+            }
+            else
+                return false;
         }
     }
 }
